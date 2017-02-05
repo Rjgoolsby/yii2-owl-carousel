ffdb8f0fe2042fa87c3dac13bc3f675c751be196
rjgoolsby <rjgoolsby1@gmail.com>
2017-02-05 16:13:24 -0500

    Updated Asset Bundle for V2.2.0

--- OwlCarouselAsset.php
+++ OwlCarouselAsset.php
@@ -9,33 +9,34 @@ use yii\web\AssetBundle;
 
 /**
  * Asset Bundle for Owl Carousel Widget
- * 
+ *
  * @author Arjun C K <ckarjun5@gmail.com>
  */
 class OwlCarouselAsset extends AssetBundle
 {
+    
     /**
      * @inheritdoc
      */
     public $css = [
-        'css/owl.carousel.css',
-        'css/owl.theme.css',
+        'css/owl.carousel.min.css',
+        'css/owl.theme.default.min.css',
     ];
-    
+
     /**
      * @inheritdoc
      */
     public $js = [
-        'js/owl.carousel.js',
+        'js/owl.carousel.min.js',
     ];
-    
+
     /**
      * @inheritdoc
      */
     public $depends = [
         'yii\web\JqueryAsset',
     ];
-    
+
     /**
      * @inheritdoc
      */
